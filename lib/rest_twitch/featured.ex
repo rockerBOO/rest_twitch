083cf0b22740039bf42a76cdbd59d39c953cc32e
rockerBOO <rockerboo@gmail.com>
2015-06-20 23:22:59 -0400

    Moving request, adding featured. Updated readme. Removing base project made file in lib

---
+++ lib/rest_twitch/featured.ex
@@ -0,0 +1,12 @@
+defmodule RestTwitch.Featured do
+  defstruct [
+      :image,
+      :text,
+      :title,
+      :sponsored,
+      :scheduled,
+      :stream,
+      :preview,
+      :_links
+  ]
+end
